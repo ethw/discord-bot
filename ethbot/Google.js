346a5cacefb25a26dedd6ef35582406b57366436
Ethan Wiberforce <ethan.wilberforce@gmail.com>
2017-03-18 19:42:46 -0700

    Add google search

--- ethbot/Google.js
+++ ethbot/Google.js
@@ -1,13 +1,22 @@
 var env = require('../config.json'),
-    GoogleImages = require('google-images');
+    GoogleImages = require('google-images'),
+    GoogleSearch = require('google-search');
 var imageClient = new GoogleImages(env.googleInfo.cseid, env.googleInfo.cseapikey);
+var searchClient = new GoogleSearch({key: env.googleInfo.cseapikey, cx: env.googleInfo.cseid});
 
 class Google {
   Message(command, message, callback) {
-    var commandIndex = message.content.indexOf(command);
-    var args = message.content.substring(commandIndex + command.length).trim();
+    var commandTermArray = message.content.split(" ")
+    var secondCommandTerm = commandTermArray[2];
+    var secondCommandIndex = message.content.indexOf(secondCommandTerm);
+    var searchString = message.content.substring(secondCommandIndex + command.length).trim();
 
-    imageClient.search(args, {page: 1, safe: 'off'}).then(images => callback(images[0].url));
+
+    if (secondCommandTerm === "search") {
+      searchClient.build({q: searchString}, (err, res) => {if (!err) callback(res.items[0].link)});
+    } else if (secondCommandTerm === "image" || secondCommandTerm === "images") {
+      imageClient.search(searchString, {page: 1, safe: 'off'}).then(images => callback(images[0].url));
+    }
   }
 }
 
