d85f5c494c191e8c2a607614c7780332357059e7
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2017-03-19 15:19:27 -0700

    Add music functionality

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var env = require('./config.json'),
     EthBot = require('./ethbot/index.js'),
-    Discord = require('discord.js');
+    Discord = require('discord.js'),
+    music = require('discord.js-music-v11');
 
 var bot = new EthBot();
 var client = new Discord.Client();
@@ -24,4 +25,10 @@ client.on('message', message => {
   }
 })
 
+//todo: replace with self-written music module. likely using youtubedl-node
+music(client, {
+  prefix: '<@' + env.botId + '> music ',
+  global: false,
+  anyoneCanSkip: true,
+});
 client.login(env.token);
